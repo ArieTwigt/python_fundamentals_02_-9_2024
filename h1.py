f951647a542c1ba5b733f6bbeb21ef8482024022
Arie Twigt <twigt.arie@gmail.com>
2024-09-02 12:24:36 +0200

    initial commit

---
+++ h1.py
@@ -0,0 +1,105 @@
+#%%
+server_name = "Server X"
+# %%
+server_name
+# %%
+user_age = 45
+# %%
+first_name = "Arie"
+last_name = "Twigt"
+
+first_name + last_name 
+# %%
+5 + 5
+# %%
+5 * first_name
+# %%
+True + True
+# %%
+True and False
+# %%
+print("Hello Arie")
+# %%
+print = "Arie"
+# %%
+print
+# %%
+print("Hello Arie")
+# %%
+my_list = ['Arie', 4000, True]
+# %%
+my_tuple = ('Arie', 4000, True)
+# %%
+my_tuple
+# %%
+my_list[1] = 5000
+# %%
+my_list
+# %%
+my_tuple[1] = 5000
+# %%
+my_tuple[0]
+
+# %%
+allowed_files = [".txt", ".csv"]
+
+#%%
+allowed_files = (".txt", ".csv")
+
+
+#%% dictionary
+person_dict = {"name": "Arie", 
+               "age": 40,
+               "hobbies": ['football', 'programming']}
+# %%
+person_dict['age']
+# %%
+person_list = ['Arie', 40, "Katwijk"]
+
+#%% 
+person_dict['hobbies']
+# %%
+person_dict['hobbies'][1]
+# %%
+my_list[3]
+# %%
+first_name
+# %%
+last_name
+# %%
+first_name + last_name
+# %%
+first_name + " " + last_name
+# %%
+f"Mijn voornaam is {first_name} en achternaam is {last_name}"
+
+# %%
+f"{first_name} {last_name}"
+
+# %%
+first_name + user_age
+# %%
+f"{first_name} {user_age}"
+# %%
+first_name.upper()
+# %%
+first_name.upper()
+# %%
+user_age.upper()
+# %%
+first_name.upper()
+# %%
+first_name.replace("r", "p")
+# %%
+my_list.append("✅")
+# %%
+my_list
+
+# %%
+first_name[0]
+
+#%%
+my_list[0]
+# %%
+person_dict['name']
+# %%
---
+++ hello.py
@@ -0,0 +1,9 @@
+print("Hello world!")
+
+#%% dit is een comment
+name = "Arie"
+
+
+# %%
+name.upper()
+# %%
